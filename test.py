54748787a0ebb96fc10beefbf872a5bf03f8629b
Legacy107 <quocmai1107@gmail.com>
2022-05-28 22:23:58 +1000

    refactor: change from multithreading to multiprocessing

--- test.py
+++ test.py
@@ -1,50 +1,59 @@
 from tsp_gen import gen_test
 from tsp_br_test import tsp_br
 from tsp_dp_test import tsp_dp
-import threading
+import concurrent.futures
+import psutil
+import time
 import os
 
-start = 3
-end = 22
-test_size = 100
+def run_br(filename, number_city, test_number):
+    runtime = tsp_br(filename)[0]
+    print(f"Completed br test {test_number} of n = {number_city}")
+    return runtime
 
-with open("result_br.txt", "a") as file_br, open("result_dp.txt", "a") as file_dp:
-    for i in range(start, end):
-        time_br = 0.0
-        time_dp = 0.0
-        br_threads = []
-        dp_threads = []
-        for j in range(test_size):
-            filename = f"input_{j}.txt"
-            gen_test(i, filename)
+def run_dp(filename, number_city, test_number):
+    runtime = tsp_dp(filename)[0]
+    print(f"Completed dp test {test_number} of n = {number_city}")
+    return runtime
 
-            def run_br(i, j):
-                global time_br
-                time_br += tsp_br(filename)[0]
-                print(f"Completed br test {j} of n = {i}")
-            
-            def run_dp(i, j):
-                global time_dp
-                time_dp += tsp_dp(filename)[0]
-                print(f"Completed dp test {j} of n = {i}")
+def main():
+    start = 10
+    end = 11
+    test_size = 20
+    batch = psutil.cpu_count(logical=False) // 2
 
-            t_br = threading.Thread(target=run_br, args=(i, j,))
-            t_dp = threading.Thread(target=run_dp, args=(i, j,))
-            br_threads.append(t_br)
-            dp_threads.append(t_dp)
+    with (
+        open("result_br.txt", "a") as file_br,
+        open("result_dp.txt", "a") as file_dp,
+        concurrent.futures.ProcessPoolExecutor() as executor
+    ):
+        for number_city in range(start, end):
+            time_br = 0.0
+            time_dp = 0.0
 
-        for t in br_threads:
-            t.start()
-        for t in br_threads:
-            t.join()
+            for batch_start in range(0, test_size, batch):
+                br_futures = []
+                dp_futures = []
 
-        for t in dp_threads:
-            t.start()
-        for t in dp_threads:
-            t.join()
+                for test_number in range(batch_start, min(batch_start + batch, test_size)):
+                    filename = f"input_{test_number}.txt"
+                    gen_test(number_city, filename)
 
-        file_br.write(f"{i} {time_br / float(test_size)}\n")
-        file_dp.write(f"{i} {time_dp / float(test_size)}\n")
+                    br_futures.append(executor.submit(run_br, filename, number_city, test_number))
+                    dp_futures.append(executor.submit(run_dp, filename, number_city, test_number))
+                
+                time_br += sum([f.result() for f in br_futures])
+                time_dp += sum([f.result() for f in dp_futures])
 
-for i in range(test_size):
-    os.remove(f"input_{i}.txt")
+            file_br.write(f"{number_city} {time_br / float(test_size)}\n")
+            file_dp.write(f"{number_city} {time_dp / float(test_size)}\n")
+
+    for number_city in range(test_size):
+        os.remove(f"input_{number_city}.txt")
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    end = time.time()
+    print(f"Finished process in {end - start} seconds")
